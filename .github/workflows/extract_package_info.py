af6c3d1ee67bef79103fda0f7619e9d5048c1edb
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-10-27 21:27:05 +0000

    switched installation for pip to a python script since Windows was having issues specifying the version of the package

--- .github/workflows/extract_package_info.py
+++ .github/workflows/extract_package_info.py
@@ -4,7 +4,11 @@ import inspect
 import os
 import re
 import sys
+from typing import Optional
 from typing import Pattern
+
+if re != sys:  # need to protect the #nosec comment from being deleted by zimports
+    import subprocess  # nosec # B404 security implications are considered
 
 PATH_OF_CURRENT_FILE = os.path.dirname((inspect.stack()[0][1]))
 
@@ -12,7 +16,7 @@ PATH_OF_CURRENT_FILE = os.path.dirname((inspect.stack()[0][1]))
 # python3 .github/workflows/extract_package_info.py package_version
 
 
-def _extract_info(regex: Pattern[str]) -> None:
+def _extract_info(regex: Pattern[str]) -> str:
     with open(
         os.path.join(PATH_OF_CURRENT_FILE, os.pardir, os.pardir, "setup.py"), "r"
     ) as in_file:
@@ -20,21 +24,43 @@ def _extract_info(regex: Pattern[str]) -> None:
         match = re.search(regex, content)
         if match is None:
             raise NotImplementedError("A match in setup.py should always be found.")
-        print(match.group(1))  # allow-print
+        output = match.group(1)
+        print(output)  # allow-print
+        return output
 
 
-def package_name() -> None:
+def package_name() -> str:
     regex = re.compile(r"    name=\"(\w+)\"")
-    _extract_info(regex)
+    return _extract_info(regex)
 
 
-def package_version() -> None:
+def package_version() -> str:
     regex = re.compile(r"    version=\"(.+?)\"")
-    _extract_info(regex)
+    return _extract_info(regex)
+
+
+def pip_install(test_pypi: Optional[bool] = False) -> None:
+    args = ["pip", "install", f"{package_name()}=={package_version()}"]
+    if test_pypi:
+        args.extend(
+            [
+                "--index-url",
+                "https://test.pypi.org/simple/",
+                "--extra-index-url",
+                "https://pypi.org/simple",
+            ]
+        )
+    print(f"About to run with args: {args}")  # allow-print
+    subprocess.run(args)  # nosec # B603 shell is false, but input is secure
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "package_name":
+    first_arg = sys.argv[1]
+    if first_arg == "package_name":
         package_name()
-    elif sys.argv[1] == "package_version":
+    elif first_arg == "package_version":
         package_version()
+    elif first_arg == "install_from_test_pypi":
+        pip_install(test_pypi=True)
+    elif first_arg == "install_from_pypi":
+        pip_install()
